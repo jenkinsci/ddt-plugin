29fe2c853f96c8db18d1d27a1712d770cd41d1bc
freaker2k7 <thenetfreaker@gmail.om>
2019-03-20 23:22:26 +0200

    Changed default uuid

--- src/main/java/io/jenkins/plugins/ddt/QADDTAPI.java
+++ src/main/java/io/jenkins/plugins/ddt/QADDTAPI.java
@@ -159,7 +159,7 @@ public class QADDTAPI {
 		
 		if (
 			uid != null && uid.length() > 0 
-			&& !uuid.equals("9b19170c2cb3f91ed03f4e38ab4c541ac306c4457f5fde20ee4ea3dbb0f26160") // sha256(':5bf0525638') # Demo UUID
+			&& !uuid.equals("9b19170c2cb3f91ed03f4e38ab4c541ac306c4457f5fde20ee4ea3dbb0f26160") // sha256(':5c928c513d') # Demo UUID
 		) {
 			is_uid = _hash(uid) + "/";
 		}
